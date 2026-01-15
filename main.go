3a867127b53bba9bfaf4ec7a29d9f91b954dc865
Ashutosh Pradhan <ashutosh.pradhan0049@gmail.com>
2026-01-15 15:07:24 +0530

    initializing server

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+
+func main() {
+
+	mux := http.NewServeMux()
+
+	server := http.Server{
+		
+		Handler: mux,
+		Addr: ":8080",
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Printf("Error: %v",err)
+	}
+
+	
+}
